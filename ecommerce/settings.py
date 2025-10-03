eb8a8b72cd5caddc39eb9f64fd4f7a56ce70417d
muppudathiganesh <muppuraj11@gmail.com>
2025-10-03 22:45:11 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -62,6 +62,8 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
+                  'django.template.context_processors.debug',
+                    'django.template.context_processors.i18n',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
