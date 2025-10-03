365bda418f99b162d0a17e25b9544717bd138147
muppudathiganesh <muppuraj11@gmail.com>
2025-10-03 23:01:09 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qfr=-t#$==vk7m1gn24q7fbu!yup)6()@43tm0gt5!9^-!!&46'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["ecomm-6-bp.onrender.com"]
 
 
 # Application definition
